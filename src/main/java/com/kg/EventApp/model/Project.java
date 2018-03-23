03f7b159f6eddf0d850de7ae20979267b751ae8b
S_K_Dinesh <34340102+Dinesh102@users.noreply.github.com>
2018-03-23 11:52:14 +0530

    Update Project.java

--- src/main/java/com/kg/EventApp/model/Project.java
+++ src/main/java/com/kg/EventApp/model/Project.java
@@ -1,1 +1,54 @@
 
+package com.kg.EventApp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Project {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long agendaId;
+
+	@Column
+	private String Sensor1;
+
+	@Column
+	private String Sensor2;
+
+	@Column
+	private String Sensor3;	
+
+	public Long getaId() {
+		return agendaId;
+	}
+
+	public void setaId(Long agendaId) {
+		this.agendaId = agendaId;
+	}
+
+	public String Sensor1() {
+		return time;
+	}
+
+	public void setSensor1(String Sensor1) {
+		this.Sensor1 = Sensor1;
+	}
+
+	public String getSensor2() {
+		return Sensor1;
+	}
+
+		public void setSensor2(String Sensor2) {
+		this.Sensor2 = Sensor2;
+	}
+
+		public String getSensor3() {
+		return Sensor3;
+	}
+	public void setSensor3(String Sensor3) {
+		this.Sensor3 = Sensor3;
+	}
+
+
+	}
